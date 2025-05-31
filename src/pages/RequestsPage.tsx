c849df4e3a6edc14fc61f2f83797f47b7d371957
Musaib Bin Bashir <musaibbashir02@gmail.com>
2025-05-31 12:43:32 +0530

    Update RequestsPage.tsx

--- src/pages/RequestsPage.tsx
+++ src/pages/RequestsPage.tsx
@@ -368,7 +368,7 @@ const RequestsPage = () => {
 
       <div className="mb-6 flex flex-col md:flex-row gap-4">
         <div className="relative flex-grow">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-event-none">
             <Search className="h-5 w-5 text-gray-400" />
           </div>
           <input
