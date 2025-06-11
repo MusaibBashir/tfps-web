a9401497193ae8bf62b2252c7d927bdf29bf8568
Musaib Bin Bashir <musaibbashir02@gmail.com>
2025-06-11 14:11:33 +0530

    Update poster location to imgur

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -601,7 +601,7 @@ const HomePage = () => {
                 {/* Image Section */}
                 <div className="relative h-64 lg:h-auto">
                   <img
-                    src="/posters/HometownHuesPoster.png"
+                    src="https://i.imgur.com/0jN8AOw.png"
                     alt="Hometown Hues Photography Competition"
                     className="w-full h-full object-cover"
                   />
